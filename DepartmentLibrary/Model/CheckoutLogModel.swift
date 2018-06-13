a0ec9ac0fbd3fea708b5046e46a668f32bb0d345
canela7 <canelabrian123@gmail.com>
2018-06-13 10:50:57 -0700

    add item constraints fixed for storyboard

---
+++ DepartmentLibrary/Model/CheckoutLogModel.swift
@@ -0,0 +1,28 @@
+//
+//  CheckoutLogModel.swift
+//  DepartmentLibrary
+//
+//  Created by Brian Canela on 6/12/18.
+//  Copyright © 2018 Brian Canela. All rights reserved.
+//
+
+import Foundation
+
+
+class CheckoutLogModel  {
+    
+    var additionalInformation = ""
+    var cin: Int
+    var studentName = ""
+    var dateBorrowed: Date!
+    var dueDate: Date?
+    
+    
+    init(cin: Int) {
+        self.cin = cin
+    }
+    
+    
+    
+    
+}
