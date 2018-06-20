7143b9d10e1da7a4b1cd9409c943091f28208aa0
canela7 <canelabrian123@gmail.com>
2018-06-19 18:58:00 -0700

    change current data from textfield to label, fixed date format in checkout section

--- DepartmentLibrary/Controller/CheckoutViewController.swift
+++ DepartmentLibrary/Controller/CheckoutViewController.swift
@@ -15,17 +15,26 @@ class CheckoutViewController: UIViewController {
     @IBOutlet weak var studentCinTextField: UITextField!
     
     
-    @IBOutlet weak var dateBorrowedTextField: UITextField!
+    @IBOutlet weak var dateBorrowedLabel: UILabel!
     
     @IBOutlet weak var dueDateTextField: UITextField!
     
-    var datepicker = UIDatePicker()
+   
     var dueDatePicker = UIDatePicker()
+    
+    //use for today date
+    var datepicker = UIDatePicker()
+    let formatter = DateFormatter()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         createDatePicker()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        let dateString = formatter.string(from: datepicker.date)
+        dateBorrowedLabel.text = dateString
 
     }
 
@@ -42,40 +51,19 @@ class CheckoutViewController: UIViewController {
 
 extension CheckoutViewController {
     func createDatePicker(){
-        //toolbar
-        let toolbar = UIToolbar()
-        toolbar.sizeToFit()
-        
+
         let toolbarDueDate = UIToolbar()
         toolbarDueDate.sizeToFit()
-        
-        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressedDatePicker))
-        toolbar.setItems([done], animated: true)
-        
+
         
         let doneDueDate = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressedDueDate))
         toolbarDueDate.setItems([doneDueDate], animated: true)
-        
-        dateBorrowedTextField.inputAccessoryView = toolbar
-        dateBorrowedTextField.inputView = datepicker
+
         
         dueDateTextField.inputAccessoryView = toolbarDueDate
         dueDateTextField.inputView = dueDatePicker
         
-        
-        datepicker.datePickerMode = .date
         dueDatePicker.datePickerMode = .date
-    }
-    
-    @objc func donePressedDatePicker() {
-        //format date
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .none
-        let dateString = formatter.string(from: datepicker.date)
-        
-        dateBorrowedTextField.text = "\(dateString)"
-        self.view.endEditing(true)
     }
     
     
